d92e004e02f10aaf21819ef9941ecde123806e6e
Caterina Barbero <caterinabarberoros@gmail.com>
2025-11-25 14:43:11 +0100

    core(domain): Add base class user and define user roles

---
+++ src/main/java/net/javaguids/lost_and_found/model/enums/UserRole.java
@@ -0,0 +1,8 @@
+package net.javaguids.lost_and_found.model.enums;
+
+// Enum defining the different roles users can have in the system
+public enum UserRole {
+    ADMIN, 
+    MODERATOR, 
+    USER 
+}
